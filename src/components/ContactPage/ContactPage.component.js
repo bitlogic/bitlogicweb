d05e65da9ffd64fe8bf2df41ba28a55e895e087c
Maximiliano Prystupczuk <maximiliano.prystupczuk@bitlogic.io>
2020-02-03 17:42:01 -0300

    Maxi

--- src/components/ContactPage/ContactPage.component.js
+++ src/components/ContactPage/ContactPage.component.js
@@ -1,43 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
 import Default from '../../layouts/Default';
 import SEO from '../seo';
+
 import './ContactPage.css';
-import { OutlinedInput, Button } from '@material-ui/core';
-import SendIcon from '@material-ui/icons/Send';
 
-const ContactPage = () => (
-  <Default className="ContactPage ContactPage__Container">
-    <SEO title="Contact" />
+const ContactPage = () => {
+  const [formData, setFormData] = useState({});
 
-    <h1 className="ContactPage__Title">
-      Get in touch
-    </h1>
-    <div className="ContactPage__Content">
-      <div className="ContactPage__LeftContent">
-        <h2 className="ContactPage__Subtitle">Dejanos tu consulta</h2>
-        <div className="ContactPage__Description">
-          Un asesor se pondra en contacto a la brevedad
+  const onFormChange = (property) => ({ target: { value } }) => {
+    setFormData({
+      ...formData,
+      [property]: value,
+    });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Default className="ContactPage ContactPage__Container">
+      <SEO title="Contact" />
+
+      <h1 className="ContactPage__Title">
+        Get in touch
+      </h1>
+      <div className="ContactPage__Content">
+        <div className="ContactPage__LeftContent">
+          <h2 className="ContactPage__Subtitle">Dejanos tu consulta</h2>
+          <div className="ContactPage__Description">
+            Un asesor se pondra en contacto a la brevedad
+        </div>
+        </div>
+
+        <div className="ContactPage__RightContent">
+          <form onSubmit={onSubmit}>
+            <TextField
+              variant="outlined"
+              placeholder="Nombre y Apellido"
+              className="ContactPage__Input"
+              value={formData.name || ''}
+              onChange={onFormChange('name')}
+              required
+            />
+            <TextField
+              variant="outlined"
+              placeholder="Teléfono"
+              className="ContactPage__Input"
+              value={formData.phone || ''}
+              onChange={onFormChange('phone')}
+            />
+            <TextField
+              variant="outlined"
+              placeholder="Correo Electrónico"
+              className="ContactPage__Input"
+              value={formData.email || ''}
+              onChange={onFormChange('email')}
+            />
+            <TextField
+              variant="outlined"
+              placeholder="Consulta" multiline={true} rows={5}
+              className="ContactPage__Input"
+              value={formData.body || ''}
+              onChange={onFormChange('body')}
+            />
+
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              endIcon={<SendIcon />}
+            >
+              Enviar
+            </Button>
+          </form>
+        </div>
+
       </div>
-      </div>
-
-      <div className="ContactPage__RightContent">
-        <OutlinedInput id="time" type="text" placeholder="Nombre y Apellido" className="ContactPage__Input" />
-        <OutlinedInput id="time" type="text" placeholder="Teléfono" className="ContactPage__Input" />
-        <OutlinedInput id="time" type="text" placeholder="Correo Electrónico" className="ContactPage__Input" />
-        <OutlinedInput id="time" type="text" placeholder="Consulta" multiline={true} rows={5} className="ContactPage__Input" />
-
-        <Button
-          variant="contained"
-          color="primary"
-          endIcon={<SendIcon/>}
-          onClick={() => { console.log('onClick'); }}
-        >
-          Enviar
-        </Button>
-      </div>
-
-    </div>
-  </Default>
-);
+    </Default>
+  );
+}
 
 export default ContactPage;
