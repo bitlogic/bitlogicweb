6a59b51b4c428cf67cb9b80bef11f8e95437b235
Joel Acef <joel.acef@bitlogic.io>
2020-03-25 13:42:57 -0300

    culture bitway refactored

--- src/components/CulturePage/CulturePage.component.js
+++ src/components/CulturePage/CulturePage.component.js
@@ -6,10 +6,31 @@ import Default from '../../layouts/Default';
 import './CulturePage.css';
 
 const PHOTOS = [
-  '/assets/photos/bitlogic_photo_11.jpg',
-  '/assets/photos/bitlogic_photo_12.jpg',
-  '/assets/photos/bitlogic_photo_3.jpg',
-  '/assets/photos/bitlogic_photo_4.jpg',
+  {
+    imageUrl: '../assets/bitway/bitway-1.jpg',
+    textImage:
+      'Somos audaces, incansables y atrevidos. Transformamos cada meta en un desafío personal que nos llena de insomnio y orgullo.',
+  },
+  {
+    imageUrl: '../assets/bitway/bitway-2.jpg',
+    textImage:
+      'Nos atrevemos a decir lo que pensamos, porque para innovar hay que  animarse a creer en uno mismo.',
+  },
+  {
+    imageUrl: '../assets/bitway/bitway-3.jpg',
+    textImage:
+      'Elegimos caminar distinto  y con un corazón audaz construir un mundo mejor.',
+  },
+  {
+    imageUrl: '../assets/bitway/bitway-4.jpg',
+    textImage:
+      'Inconformistas de nacimiento salimos al mundo a desafiarlo.  Somos de los que dicen y hacen,  en casa nos enseñaron a cumplir nuestras promesas.',
+  },
+  {
+    imageUrl: '../assets/bitway/bitway-5.jpg',
+    textImage:
+      'Nos animamos a cruzar límites, a no ir por el camino amarillo, a buscar mil veces a Wally.',
+  },
 ];
 
 const CulturePage = () => {
@@ -56,37 +77,41 @@ const CulturePage = () => {
   return (
     <Default className="CulturePage CulturePage__Container">
       <SEO title="Culture" />
-      <h1 className="CulturePage__Title">{culturePageData.title}</h1>
-      <div className="CulturePage__Content">
-        <div>
-          <div
-            className="CulturePage__Description"
+      <div className="CulturePage TitleContent">
+        <h1 className="CulturePage__Title">{culturePageData.title}</h1>
+        <div className="CulturePage__FirstText">
+          <p
             dangerouslySetInnerHTML={{ __html: culturePageData.description }}
-          />
-          <div className="CulturePage__Manifest">
-            <div className="CulturePage__Manifest__List">
-              <ul>
-                {culturePageData.manifestItems.map((item, i) => (
-                  <li key={i}>{item}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="CulturePage__Description">
-            <strong>Somos distintos.</strong>
-          </div>
+          ></p>
+          <span>SOMOS DISTINTOS</span>
+          <br />
+          <a target="_blank" href={culturePageData.link}>
+            Join us >>>
+          </a>
         </div>
-        <div>
-          <div className="CulturePage__ImagesContainer">
-            {PHOTOS.map((imageSrc, i) => (
-              <img key={i} src={imageSrc} alt={`bitlogic #${i}`} />
-            ))}
-          </div>
-          <div className="CulturePage__Link">
-            <a target="_blank" href={culturePageData.link}>
-              >> Jobs
-            </a>
-          </div>
+      </div>
+      <div className="CulturePage__Content">
+        <div className="CulturePage__ImagesContainer">
+          {PHOTOS.map((imageSrc, i) => (
+            <img
+              key={i}
+              src={imageSrc.imageUrl}
+              alt={imageSrc.textImage}
+              className={`CulturePage__Image CulturePage__Image--${
+                photoIndex === i ? 'active' : 'hidden'
+              }`}
+            />
+          ))}
+          {PHOTOS.map((imageSrc, i) => (
+            <h4
+              key={i}
+              className={`CulturePage__Image textoSlider CulturePage__Image--${
+                photoIndex === i ? 'active' : 'hidden'
+              }`}
+            >
+              {imageSrc.textImage}
+            </h4>
+          ))}
         </div>
       </div>
     </Default>
