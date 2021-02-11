56dc98dbd27d6b5acdc4727e2412dd35c3399ee6
Federico Aguirre <federico.aguirre@bitlogic.io>
2021-02-10 21:37:07 -0300

    fix contact form

--- src/components/ContactPage/ContactPage.component.js
+++ src/components/ContactPage/ContactPage.component.js
@@ -17,17 +17,50 @@ import './ContactPage.css';
 const ContactPage = () => {
   const [formData, setFormData] = useState({});
 
+  const {
+    allContactJson: { nodes },
+  } = useStaticQuery(graphql`
+    query ContactPage {
+      allContactJson {
+        nodes {
+          title
+          description
+          section
+        }
+      }
+    }
+  `);
+
+  if (!nodes || !nodes.length) {
+    return null;
+  }
+
+  const contactPageData = nodes[0];
+
   return (
     <Default className="ContactPage ContactPage__Container">
       <SEO title="Contact" />
-      <div id="formhs">
-        <HubspotForm
-          portalId="8668423"
-          formId="b907e41f-0c77-4a8c-ad1c-0cf155ea2eb2"
-          onSubmit={() => console.log('Submit!')}
-          onReady={(form) => console.log('Form ready!')}
-          loading={<div>Loading...</div>}
-        />
+
+      <h1 className="ContactPage__Title">{contactPageData.section}</h1>
+      <div className="ContactPage__Content">
+        <div className="ContactPage__FormContainer">
+          <div className="ContactPage__LeftContent">
+            <h2 className="ContactPage__Subtitle">{contactPageData.title}</h2>
+            <div className="ContactPage__Description">
+              {contactPageData.description}
+            </div>
+          </div>
+
+          <div className="ContactPage__RightContent">
+            <HubspotForm
+              portalId="8668423"
+              formId="b907e41f-0c77-4a8c-ad1c-0cf155ea2eb2"
+              onSubmit={() => console.log('Submit!')}
+              onReady={(form) => console.log('Form ready!')}
+              loading={<div>Loading...</div>}
+            />
+          </div>
+        </div>
       </div>
     </Default>
   );
